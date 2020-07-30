46cd7c16a0189aee6090784ce53df3bdbb597775
hkattt <2690133@schoolsnet.act.edu.au>
2020-07-30 16:55:07 +1000

    added the images, starting to make piece classes

--- pieces.py
+++ pieces.py
@@ -1,1 +1,11 @@
 # PIECE CLASSES
+
+class Piece():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class King(Piece):
+    def __init__(self, x, y):
+        super().__init__(x, y):
+        
