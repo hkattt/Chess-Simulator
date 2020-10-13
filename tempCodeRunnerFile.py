1124e8301f569b5ea5443490378c50d2d63fc768
hkattt <2690133@schoolsnet.act.edu.au>
2020-10-13 22:21:30 +1100

    deleted a randomly made file

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-    if failsafe_check_backward_blue and failsafe_check_forward_blue >= blue_lowers[piece_index]:
-                                                        consistencies[piece_index] = True
