39eb9422875a2f158e897193419b64aad2ee1104
hkattt <2690133@schoolsnet.act.edu.au>
2020-11-05 14:59:35 +1100

    documentation on the arduino code

--- reveive/receive.ino
+++ reveive/receive.ino
@@ -1,14 +1,19 @@
-String data = "";
+// string to store incoming data
+String data = ""; 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
+  // opens the serial monitor
   Serial.begin(9600);  
 }
 
 void loop() {
+  // checks if data is being received 
   if (Serial.available() > 0) {
+    // stores incoming data in the data variable
     data = Serial.readString();
+    // indexing the data (this was for testing purposes)
     if (data[0] == '0') {
       digitalWrite(LED_BUILTIN, HIGH);
       delay(3000); 
